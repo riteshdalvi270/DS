c31d834ee881906db8380acdb1dda9aa87ea1d04
Dalvi,Ritesh <Ritesh.Dalvi@Cerner.com>
2017-03-16 19:15:05 -0500

    Started array list

---
+++ src/main/java/crackingcode6th/arraylist/ArrayList.java
@@ -0,0 +1,15 @@
+package crackingcode6th.arraylist;
+
+/**
+ * @author Ritesh Dalvi (rd026600).
+ */
+public class ArrayList<T>
+{
+    private T array[];
+    private int size;
+
+    public ArrayList(int size)
+    {
+        array = new T[size];
+    }
+}
